8e7641fbf3d35f7afdfb360ae37e6d4e62501b22
Muhammad Yafi <yafithekid212@gmail.com>
2015-09-10 23:19:56 +0700

    first commit

---
+++ src/main/java/com/if4031/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.if4031;
+//unused import needed for hello world test
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+
+}
